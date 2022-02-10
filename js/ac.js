f538031bb486fd42450e6b1828b7cf7568762ddb
unknown <you@example.com>
2022-02-10 12:14:04 +0600

    Input validation

--- js/ac.js
+++ js/ac.js
@@ -13,7 +13,7 @@ const withdrawInput = document.getElementById('withdraw_input');
 const withdrawBtn = document.getElementById('withdraw_btn');
 
 depositBtn.addEventListener('click', function () {
-  if (depositInput.value == '') { alert('ভুলবাল ইনপুট') } else {
+  if (depositInput.value == '' || typeof depositInput.value != 'number') { alert('ভুলবাল ইনপুট') } else {
     todayDeposit.innerText = parseFloat(todayDeposit.innerText) + parseFloat(depositInput.value);
     totalDeposit.innerText = parseFloat(totalDeposit.innerText) + parseFloat(depositInput.value);
     totalBalance.innerText = parseFloat(totalBalance.innerText) + parseFloat(depositInput.value);
@@ -22,7 +22,9 @@ depositBtn.addEventListener('click', function () {
 })
 
 withdrawBtn.addEventListener('click', function () {
-  if (withdrawInput.value == '' || totalBalance.innerText < withdrawInput.value) { alert('ভুলবাল ইনপুট') } else {
+  if (withdrawInput.value == '' || typeof withdrawInput.value != 'number' || totalBalance.innerText < withdrawInput.value) {
+    alert('ভুলবাল ইনপুট')
+  } else {
     todayWithdraw.innerText = parseFloat(todayWithdraw.innerText) + parseFloat(withdrawInput.value);
     totalBalance.innerText = totalBalance.innerText - withdrawInput.value;
     totalWithdraw.innerText = parseFloat(totalWithdraw.innerText) + parseFloat(withdrawInput.value);
